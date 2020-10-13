8f1883638024b1ecf6b95f426299e3674a287739
Rod Hentringer <rodhentringer@gmail.com>
2020-10-13 12:15:42 -0400

    app.js set up

---
+++ app.js
@@ -0,0 +1,9 @@
+const todoInput = document.querySelector('.todo-input')
+const todoButton = document.querySelector('todo-button')
+const todoList = document.querySelector('todo=list')
+
+todoButton.addEventListener('click', addTodo)
+
+function add(event) {
+	console.log('hello')
+}
